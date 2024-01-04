408029581cc01796bd85aee046fa5d687e1d2244
Carlos Tavares <cmmt08@gmail.com>
2024-01-04 12:46:45 -0300

    realizei algumas alteracoes

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,15 +2,17 @@
 
 context('Funcionalidade Login', () =>{
 
-    it('Deve fazer login com sucesso', () =>{
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    
+    it('Deve fazer login com sucesso', () =>{
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()   
     })
 
     it('Deve exibir uma mensagem de erro', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('xxxx@xxxx.com')
         cy.get('#password').type('xxxx@xxxx')
         cy.get('.woocommerce-form > .button').click()
